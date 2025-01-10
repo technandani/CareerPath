b6f33946725d5c20b434ca5f47612fd7da2ecd26
Nandani <nks854338@gmail.com>
2025-01-10 07:57:48 +0530

    commited

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import QuizHeader from "../components/quiz/QuizHeader";
-import QuizList from "../components/quiz/QuizList";
+import QuizHeader from "../Components/quiz/QuizHeader";
+import QuizList from "../Components/quiz/QuizList";
 import Navbar from "../Components/common/Navbar";
 import { useQuizContext } from "../context/QuizContext";
 
