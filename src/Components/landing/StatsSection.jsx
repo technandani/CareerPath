3495b05a9a3822851b5c9ace481bc10e7beca443
Nandani <nks854338@gmail.com>
2025-01-26 04:38:02 +0530

    3D board added

--- src/Components/landing/StatsSection.jsx
+++ src/Components/landing/StatsSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import BoardThree from "../three/BoardThree";
+import BoardThree from "../three/BoardThree";
 import CountUp from "react-countup";
 
 const StatsSection = () => {
@@ -13,7 +13,7 @@ const StatsSection = () => {
           The CareerPath platform is designed to track and enhance your skills, using AI-powered tools to guide you through personal and professional development.
         </p>
         <div className="mb-12">
-          {/* <BoardThree /> */}
+          <BoardThree />
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 mt-12 md:grid-cols-4 gap-8 text-white">
           <div>
